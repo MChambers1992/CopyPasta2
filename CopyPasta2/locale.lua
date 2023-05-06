18473cd371f7097a48b1a4db5729a3357afda33a
Michael Chambers <michael.chambers1992@gmail.com>
2023-05-06 08:19:34 +0100

    [Minor] Update for Patch 10.1 Dragonflight (#7)

--- CopyPasta2/locale.lua
+++ CopyPasta2/locale.lua
@@ -1,7 +1,7 @@
 -- my custom locale file - more streamlined than AceLocale and no lib dependency
 
 -- To help with missing translations please go here:
-local url = "https://wow.curseforge.com/projects/copypasta/localization/"
+local url = "https://www.tukui.org/addons.php?id=186"
 
 local addonName, vars = ...
 local Ld, La = {}, {}
